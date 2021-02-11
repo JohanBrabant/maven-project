8d8f133f8d819fa51c228ea8c23a35949d2d3a2d
JohanBrabant <76204814+JohanBrabant@users.noreply.github.com>
2021-02-11 10:45:18 +0100

    Update nummer.sh

--- .github/workflows/nummer.sh
+++ .github/workflows/nummer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Dit is een script van Github."
+echo "Dit is een script van Github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "Het antwoord is: $NUMBER"
 echo "Heeft het gewerkt?"
